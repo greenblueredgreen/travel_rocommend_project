886493a12201944704b15480d4c6ce9ee6bb7b3f
ahyun <kaeruruk@naver.com>
2024-09-11 23:04:29 +0900

    스프링 부트 연동 성공

--- Front_End/src/App.js
+++ Front_End/src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MBTITest from "./MBTITest";
 import Signup from "./Signup";
 import Login from "./Login";
 import Main from "./Main";
 
+
 const App = () => {
+
+  const [hello, setHello] = useState('')
+
+   useEffect(() => {
+       axios.get('/api/hello')
+       .then(response => setHello(response.data))
+       .catch(error => console.log(error))
+   }, []);
+
   return (
     <Router>
       <Routes>
@@ -14,7 +25,11 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<Main />} />
       </Routes>
+       <div>
+           백엔드에서 가져온 데이터입니다 : {hello}
+       </div>
     </Router>
+    
   );
 };
 
