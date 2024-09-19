87fd7b81585ba12a35f12e2ce5576acb52dd55de
ahyun <kaeruruk@naver.com>
2024-09-19 19:14:27 +0900

    /write페이지에서 /main페이지로 이동시키기! (파라미터 email도 같이 넘김)

--- Front_End/src/WritingForm.jsx
+++ Front_End/src/WritingForm.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate} from "react-router-dom";
 
 const WritingForm = () => {
   const location = useLocation();
+  const navigate = useNavigate(); // 여기서 useNavigate() 호출하여 navigate 변수에 할당
   const email = location.state?.email;
   //alert(email);
   const [subject, setSubject] = useState('');
   const [content, setContent] = useState('');
+
+  const handleClear = () => {
+    setSubject(''); // 제목 초기화
+    setContent(''); // 내용 초기화
+  };
 
   // 저장 버튼 클릭 시 호출되는 함수
   const handleSave = async () => {
@@ -25,6 +31,7 @@ const WritingForm = () => {
 
       if (response.ok) {
         alert('글이 저장되었습니다!');
+        navigate('/main', { state: { email: email } });
       } else {
         alert('저장 중 오류가 발생했습니다.');
       }
@@ -34,6 +41,10 @@ const WritingForm = () => {
     }
   };
 
+  //main페이지로 이동하는 버튼 만들기
+  const handleNavigateToMain = () => {
+    navigate('/main'); // /main 경로로 이동
+  };
 
   return (
     <div className="container mt-4">
@@ -77,6 +88,7 @@ const WritingForm = () => {
                 type="button"
                 id="clearBtn"
                 className="btn btn-secondary me-2"
+                onClick={handleClear} // 이 부분에서 handleClear 함수 연결
               >
                 모두 지우기
               </button>
@@ -87,6 +99,13 @@ const WritingForm = () => {
                 onClick={handleSave}  // 저장 버튼에 handleSave 연결
               >
                 저장
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary ms-2"
+                onClick={handleNavigateToMain} // 새 버튼 클릭 시 메인화면으로 이동
+              >
+                main화면으로 이동
               </button>
             </div>
           </div>
