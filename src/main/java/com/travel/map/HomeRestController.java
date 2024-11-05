9c5a8d4756669671dfd14fcec4cccf52c473b9fe
ahyun <kaeruruk@naver.com>
2024-11-05 11:43:48 +0900

    주소 검색 기반 맛집 추천 하는중. 백엔드로 fetch 하는중

--- src/main/java/com/travel/map/HomeRestController.java
+++ src/main/java/com/travel/map/HomeRestController.java
@@ -24,8 +24,22 @@ public class HomeRestController {
 	private String appKey = "8323252951a97b41155cd927ab433d7c";
 
 	// react로 보내는 post코드
+	// 현재 위치 기반 맛집
 	@PostMapping("/recommend")
 	public ResponseEntity<String> getLunchRecommendations(@RequestBody Map<String, String> request) {
+		String latitude = request.get("latitude");
+		String longitude = request.get("longitude");
+		String page = request.get("page");
+		String appKey = "8323252951a97b41155cd927ab433d7c"; // Kakao API Key
+
+		//알고보니 latitude와 longtiude 파라미터 위치가 바껴서 들어가 현재 위치 맛집이 출력이 안됨.
+		//파라미터 순서에 주의해서 작성.
+		return lunchService.getSearchLunchList(latitude, longitude, page, "15", appKey);
+	}
+	
+	// 주소 검색 기반 맛집 추천
+	@PostMapping("/recommend/findAddress")
+	public ResponseEntity<String> getLunchRecommendationsByAddress(@RequestBody Map<String, String> request) {
 		String latitude = request.get("latitude");
 		String longitude = request.get("longitude");
 		String page = request.get("page");
